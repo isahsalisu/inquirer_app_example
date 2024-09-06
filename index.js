a732f0d87cc81321492f215e56cfd273db20d0b6
secondaccount <quantumdev040@gmail.com>
2024-09-06 02:18:31 +0100

    Initial inquirer

---
+++ index.js
@@ -0,0 +1,22 @@
+const inquirer = require('inquirer');
+const fs = require('fs');
+
+inquirer.prompt([
+    {
+        name: 'file_name',
+        message: 'What wouldyou like the file name to be?'
+    },
+
+    {
+        name:'content',
+        message: 'What content would you like to add to the file?'
+
+    }
+]).then(data => {
+    console.log(data);
+    fs.writeFile(data.file_name + '.txt', data.content, (err) => {
+        if(err) return console.log(err);
+        console.log(`You have succefully created the ${data.file_name}  file`)
+        console.log(`The content of the file are:\n ${data.content}  content`)
+    })
+});
